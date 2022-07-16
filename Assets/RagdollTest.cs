6c66fde8a8c978b7701eef9996a2d6ddfd77811d
AnastasiaCris <58260499+OGdB@users.noreply.github.com>
2022-07-16 12:53:49 +0200

    Update RagdollTest.cs

--- Assets/RagdollTest.cs
+++ Assets/RagdollTest.cs
@@ -1,23 +1,45 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class RagdollTest : MonoBehaviour
 {
     private Rigidbody rb;
+    private NavMeshAgent agent;
+    private EnemyStateMachine esm;
     public float force = 100f;
-    private void Start() => rb = GetComponent<Rigidbody>();
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateUpAxis = false;
+        agent.updateRotation = false;
+
+        rb = GetComponent<Rigidbody>();
+        esm = GetComponent<EnemyStateMachine>();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            print("boink");
-            GetComponent<EnemyStateMachine>().enabled = false;
-            GetComponent<Rigidbody>().isKinematic = false;
-            GetComponent<NavMeshAgent>().updateUpAxis = false;
-            GetComponent<NavMeshAgent>().enabled = false;
+            esm.enabled = false;
+            rb.isKinematic = false;
+            agent.enabled = false;
 
             rb.AddForce(Vector3.up * force, ForceMode.Impulse);
-            rb.AddTorque(transform.forward * force, ForceMode.Impulse);
+            rb.AddTorque(Random.rotation.eulerAngles * force, ForceMode.Impulse);
+            
+            _ = StartCoroutine(WaitForLanded());
         }
+    }
+
+    private IEnumerator WaitForLanded()
+    {
+        yield return new WaitForSeconds(0.25f);
+        yield return new WaitUntil(() => rb.angularVelocity.magnitude < 0.05f);
+
+        rb.isKinematic = true;
+        agent.enabled = true;
+        esm.enabled = true;
     }
 }
