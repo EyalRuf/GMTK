dde7e2c6ba37e859bd5f875213da3328bd5c305d
Marre246810 <marrethegamer03@gmail.com>
2022-07-16 15:32:50 +0200

    Done with Timer / Health
    Done with some UI, but probably needs some adjusting and fixing

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -15,5 +15,30 @@ public class Unit : MonoBehaviour {
     public int maxDiceNumber;
     public int currentDiceNumber;
 
+    private bool invincible = false;
+
+    public bool Damage(int amount) {
+        if (!invincible)
+            currentHealth -= amount;
+
+        if (currentHealth <= 0)
+            return true;
+        else 
+            return false;
+    }
+
+    public void Heal(int amount) {
+        currentHealth += amount;
+    }
+
+    private void MakeInvincible(int amountOfSeconds) {
+        _ = StartCoroutine(InvincibleTimer());
+
+        IEnumerator InvincibleTimer() {
+            invincible = true;
+            yield return new WaitForSeconds(amountOfSeconds);
+            invincible = false;
+        }
+    }
 
 }
