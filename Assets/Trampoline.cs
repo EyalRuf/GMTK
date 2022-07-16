0d22d711c688031a4041ed4815e8f4bd8551720c
AnastasiaCris <58260499+OGdB@users.noreply.github.com>
2022-07-16 13:34:54 +0200

    diceroller

---
+++ Assets/Trampoline.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out DiceRoller diceRoller))
+            diceRoller.RollDice();
+    }
+}
